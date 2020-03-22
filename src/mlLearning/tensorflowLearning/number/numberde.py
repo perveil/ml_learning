14f8b67dfbbf03756c5547db0c5e03cf844639a1
perveil <986778566@qq.com>
2020-03-22 18:57:10 +0800

    3/22

--- src/mlLearning/tensorflowLearning/number/numberde.py
+++ src/mlLearning/tensorflowLearning/number/numberde.py
@@ -20,13 +20,15 @@ def max_pool_2x2(x):
 
 x = tf.placeholder(tf.float32,[None , 784])
 y_ = tf.placeholder(tf.float32,[None , 10]) #输出
-x_image = tf.reshape(x,[-1,28,28,-1]) #横向数组向矩阵转化
+x_image = tf.reshape(x,[-1,28,28,1]) #1*784横向数组向28*28*1 矩阵转化
 
+#卷积第一层
 W_conv1 = weight_variable([5, 5, 1, 32])  #定义32个5*5的filter
 b_conv1=bias_variable([32])
 h_conv1=tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1)
 h_pool1=max_pool_2x2(h_conv1) #第一层的pooling
 
+#卷积第二层
 W_conv2 = weight_variable([5, 5, 32, 64]) #定义64个5*5*32的filter
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
@@ -35,7 +37,7 @@ h_pool2 = max_pool_2x2(h_conv2)
 #全连接层
 W_fc1=weight_variable([7*7*64,1024])
 b_fc1=bias_variable([1024])
-h_pool2_flat=tf.reshape(h_pool2,[-1,7*7*64]) #reduce 成为x*(7*7*64)
+h_pool2_flat=tf.reshape(h_pool2,[-1,7*7*64]) #reduce 成为 x*(7*7*64)
 h_fc1=tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
 
 #dropout 层防止overfit
